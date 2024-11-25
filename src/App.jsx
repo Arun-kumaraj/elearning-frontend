7d5960681cccaa707493f01f21e599e60635759a
ARUN KUMAR.B <94543466+Arun-kumaraj@users.noreply.github.com>
2024-11-25 10:15:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./App.css";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Fixed typo in react-router-dom
-import Home from './pages/home/home';
+import Home from './pages/home/Home';
 import Header from './components/header/header';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/register';
